51be3916023e19192e6292ac1bd0728a2acd0880
jord97@korea.edu <jord97@korea.ac.kr>
2024-05-17 16:50:34 +0900

    Add project files.

---
+++ MandelMesh/MandelMeshInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace MandelMesh
+{
+    public class MandelMeshInfo : GH_AssemblyInfo
+    {
+        public override string Name => "MandelMesh";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("6de466a4-a2f1-43db-a5e0-c865a09403dd");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
---
+++ MandelMesh/Quaterion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace MandelMesh.Quaternion
+{
+    public class Quaternion
+    {
+        public double a {get; set;}
+        public double b { get; set; }
+        public double c { get; set; }
+        public double d { get; set; }
+
+        Quaternion(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d; 
+        }
+
+        public Point3d Extract_Point(int h)
+        {
+            Point3d extract = new Point3d();
+
+            if (h == 1) { extract = new Point3d(this.b, this.c, this.d); }
+            else if (h == 2) { extract = new Point3d(this.a, this.c, this.d); }
+            else if (h == 3) { extract = new Point3d(this.a, this.b, this.d); }
+            else if (h == 4) { extract = new Point3d(this.a, this.b, this.c); }
+            else { extract = new Point3d(0, 0, 0); }
+
+            return extract;
+        }
+
+        public static Quaternion operator +(Quaternion A, Quaternion B)
+        {
+            return new Quaternion(
+            A.a + B.a,
+            A.b + B.b,
+            A.c + B.c,
+            A.d + B.d);
+        }
+        public static Quaternion operator *(Quaternion A, Quaternion B)
+        {
+            return new Quaternion(
+                (A.a * B.a) - (A.b * B.b) - (A.c * B.c) - (A.d * B.d),
+                (A.a * B.b) + (A.b * B.a) + (A.c * B.d) - (A.d * B.c),
+                (A.a * B.c) - (A.b * B.d) + (A.c * B.a) + (A.d * B.b),
+                (A.a * B.d) + (A.b * B.c) - (A.c * B.b) + (A.d * B.a)
+                );
+        }
+
+        public static Quaternion operator ^(Quaternion A, int n)
+        {
+            for (int x=a; x>0; --x)
+            {
+                this.Multiply(this);
+            }
+            return this; 
+
+        }
+
+    }
+}
